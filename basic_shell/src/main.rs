cf66aaa944cc22d278b43f179c7405ff2c6ade63
Nathan <nslee333@gmail.com>
2022-10-05 09:08:15 -0700

    Cleared test and rebuild basic_shell

--- basic_shell/src/main.rs
+++ basic_shell/src/main.rs
@@ -1,16 +1,22 @@
 use std::env;
+use std::env::args;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
 use std::process::{Command, Child, Stdio};
 
 fn main() {
-   loop { 
-        print!("> ");
-        stdout().flush();
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+   loop {
+    print!("> ");
+    stdout().flush().unwrap();
 
-        let mut parts = input.trim().split_whitespace();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let mut commands = input.trim().split(" | ").peekable();
+    let mut previous_command = None;
+
+    while let Some(command) = commands.next() {
+        let mut parts = command.trim().split_whitespace();
         let command = parts.next().unwrap();
         let args = parts;
 
@@ -21,33 +27,14 @@ fn main() {
                 if let Err(e) = env::set_current_dir(&root) {
                     eprintln!("{}", e);
                 }
+
+                previous_command = None;
             },
-            "exit" => return,
+            "exit" = return,
             command => {
-                let stdin = previous_command
-                    .map_or(Stdio::inherit(),
-                        |output: Child| Stdio::from(output.stdout.unwrap()));
-
-                let stdout = if command.peek().is_some() {
-                    Stdio::piped()
-                } else {
-                    Stdio::inherit()
-                };
-
-                let output = Command::new(command)
-                    .args(args)
-                    .stdin(stdin)
-                    .stdout(stdout)
-                    .spawn();
-
-                match output {
-                    Ok(output) => { previous_command = Some(output); },
-                    Err(e) => {
-                        previous_command = None;
-                        eprintln!("{}", e);
-                    },
-                };
+                
             }
         }
     }
+   }
 }
