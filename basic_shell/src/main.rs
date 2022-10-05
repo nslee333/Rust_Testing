e30400aefc4d1e17a50ace47364f6909bbe53962
Nathan <nslee333@gmail.com>
2022-10-04 18:55:42 -0700

    More work on main.rs

--- basic_shell/src/main.rs
+++ basic_shell/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Command, Child, Stdio};
+
 fn main() {
-    println!("fl!");
+   loop { 
+        print!("> ");
+        stdout().flush();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        Command::new(command)
+            .spawn()
+            .unwrap();
+
+        child.wait();
+    }
 }
