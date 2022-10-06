343a189a3492618cd325114a8d78c606d1167045
Nathan <nslee333@gmail.com>
2022-10-05 18:23:00 -0700

    Adding functions.

--- DNS/dns_server/src/main.rs
+++ DNS/dns_server/src/main.rs
@@ -4,6 +4,7 @@ pub struct BytePacketBuffer {
 }
 
 impl BytePacketBuffer {
+    // Init fn.
     pub fn new() -> BytePacketBuffer {
         BytePacketBuffer {
             buf: [0; 512],
@@ -11,9 +12,40 @@ impl BytePacketBuffer {
         }
     }
 
+    // Buffer position.
     fn pos(&self) -> usize {
         self.pos
     }
 
-    fn
+    // Move position by a given amount.
+    fn step(&mut self, steps: usize) -> Result<()> {
+        self.pos += steps;
+
+        Ok(())
+    }
+
+    // Change the buffer position.
+    fn seek(&mut self, pos: usize) -> Result<()> {
+        self.pos = pos;
+
+        Ok(())
+    }
+
+    // Read a single byte and move the position one step forward.
+    fn read(&mut self) -> Result<u8> {
+        if self.pos >= 512 {
+            return Err("End of buffer".into());
+        }
+        let res = self.buf[self.pos];
+        self.pos += 1;
+
+        Ok(res)
+    }
+
+    fn get(&mut self, pos: usize) -> Result<u8> {
+        if pos >= 512 {
+            return Err("End of buffer".into());
+        }
+        Ok(self.buf[pos])
+    }
 }
