1f57e588fddada040f6d4145456d22653a090aee
Nathan <nslee333@gmail.com>
2022-10-05 19:05:46 -0700

    Building in logic, rust generated files.

--- DNS/dns_server/src/main.rs
+++ DNS/dns_server/src/main.rs
@@ -3,6 +3,9 @@ pub struct BytePacketBuffer {
     pub pos: usize,
 }
 
+
+// !! Consider converting '.into();' to '.from();' !!
+
 impl BytePacketBuffer {
     // Init fn.
     pub fn new() -> BytePacketBuffer {
@@ -62,5 +65,49 @@ impl BytePacketBuffer {
         Ok(res)
     }
 
-    
+    fn read_u32(&mut self) -> Result<u32> {
+        let res = ((self.read()? as u32) << 24)
+            | ((self.read()? as u32) << 16)
+            | ((self.read()? as u32) << 8)
+            | ((self.read()? as u32) << 0);
+
+        Ok(res)
+    }
+
+    fn read_qname(&mut self, outstr: &mut String) -> Result<()> {
+        let mut pos = self.pos();
+
+        let mut jumped = false;
+        let max_jumps = 5;
+        let mut jumps_performed = 0;
+
+        let mut delim = "";
+        
+        loop {
+            if jumpes_performed > max_jumps {
+                return Err(format!("Limit of {} jumps exceeded.", max_jumps).into());
+            }
+
+            let len = self.get(pos)?;
+
+            if (len & 0xC0) == 0xC0 {
+                if !jumped {
+                    self.seek(pos + 2);
+                }
+
+                let b2 = self.get(pos + 1)? as u16;
+                let offset = (((len as u16) ^ 0xC0)) | b2;
+                pos = offset as usize;
+
+                jumped = true;
+                jumps_performed += 1;
+
+                continue;
+            }
+        }
+
+
+    }
+
+
 }
---
+++ DNS/dns_server/target/debug/deps/dns_server-e4f6adee6a050cde.d
@@ -0,0 +1,5 @@
+/home/nslee333/Program/Rust/DNS/dns_server/target/debug/deps/dns_server-e4f6adee6a050cde.rmeta: src/main.rs
+
+/home/nslee333/Program/Rust/DNS/dns_server/target/debug/deps/dns_server-e4f6adee6a050cde.d: src/main.rs
+
+src/main.rs:
